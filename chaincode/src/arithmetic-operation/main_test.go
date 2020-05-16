b2608020faf8fce58b61c9b214880170756b0d27
Shohei Kakei <kakei.shohei@nitech.ac.jp>
2020-05-16 19:13:22 +0900

    Added test of main.go

---
+++ chaincode/src/arithmetic-operation/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "fmt"
+    "testing"
+
+    "github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+/*
+* TestInvokeInitVehiclePart simulates an initVehiclePart transaction on the CarDemo cahincode
+ */
+func TestInvokeInitArithmeticOperation(t *testing.T) {
+    fmt.Println("Entering TestInvokeInitArithmeticOperation")
+
+    // Instantiate mockStub using CarDemo as the target chaincode to unit test
+    stub := shim.NewMockStub("mockStub", new(ArithmeticOperation))
+    if stub == nil {
+        t.Fatalf("MockStub creation failed")
+    }
+
+    resultA := stub.MockInvoke(
+        "001",
+        [][]byte{
+            []byte("set"),
+            []byte("A"),
+            []byte("Alice"),
+        },
+    )
+
+    resultB := stub.MockInvoke(
+        "001",
+        [][]byte{
+            []byte("set"),
+            []byte("B"),
+            []byte("Bob"),
+        },
+    )
+
+    if resultA.Status != shim.OK {
+        t.Fatalf("Expected unauthorized user error to be returned")
+    }
+
+    if resultB.Status != shim.OK {
+        t.Fatalf("Expected unauthorized user error to be returned")
+    }
+
+    valAsbytes, err := stub.GetState("A")
+    if err != nil {
+        t.Errorf("Failed to get state (key:A)")
+    } else if valAsbytes == nil {
+        t.Errorf("Asset not found (key:A)")
+    }
+
+    val := string(valAsbytes)
+
+    if val != "Alice" {
+        t.Errorf("key:A,return:%s,expect:%s", val, "Alice")
+    }
+}
